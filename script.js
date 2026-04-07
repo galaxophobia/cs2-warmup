7c6d8d1eeab8e651d8c185c9ba8af92d6559a97f
galaxophobia <brightflowing@icloud.com>
2026-04-07 15:25:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+const btn = document.getElementById("startBtn");
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+let targets = [];
+let score = 0;
+
+btn.onclick = () => {
+  canvas.style.display = "block";
+  canvas.requestPointerLock();
+  spawnTarget();
+  gameLoop();
+};
+
+function spawnTarget() {
+  targets = [{
+    x: Math.random() * 700,
+    y: Math.random() * 500,
+    size: 30
+  }];
+}
+
+canvas.onclick = (e) => {
+  const rect = canvas.getBoundingClientRect();
+  const mx = e.clientX - rect.left;
+  const my = e.clientY - rect.top;
+
+  targets.forEach(t => {
+    if (
+      mx > t.x &&
+      mx < t.x + t.size &&
+      my > t.y &&
+      my < t.y + t.size
+    ) {
+      score++;
+      spawnTarget();
+    }
+  });
+};
+
+function gameLoop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  targets.forEach(t => {
+    ctx.fillStyle = "red";
+    ctx.fillRect(t.x, t.y, t.size, t.size);
+  });
+
+  ctx.fillStyle = "white";
+  ctx.fillText("Score: " + score, 10, 20);
+
+  requestAnimationFrame(gameLoop);
+}
